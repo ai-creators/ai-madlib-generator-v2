98686f4987e0ec326a8fe0811099820a3014472e
admclamb <adylanmclamb@yahoo.com>
2026-04-04 21:11:06 -0400

    feat: initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,10 @@
 import { type Metadata } from "next";
-import { Geist } from "next/font/google";
+import { Geist, Inter } from "next/font/google";
 
 import { TRPCReactProvider } from "@/trpc/react";
+import { cn } from "@/lib/utils";
+
+const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
   title: "Create T3 App",
@@ -18,7 +21,7 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${geist.variable}`}>
+    <html lang="en" className={cn(geist.variable, "font-sans", inter.variable)}>
       <body>
         <TRPCReactProvider>{children}</TRPCReactProvider>
       </body>
