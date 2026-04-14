1c5178e4f7280a26b1452f40588506709bd29f98
admclamb <adylanmclamb@yahoo.com>
2026-04-13 21:57:17 -0400

    Update format date

---
+++ src/lib/date.ts
@@ -0,0 +1,19 @@
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+
+dayjs.extend(relativeTime);
+
+export function formatAdlibDate(date: Date | string): string {
+  const d = dayjs(date);
+  const now = dayjs();
+  const diffDays = now.diff(d, "day");
+  const diffYears = now.diff(d, "year");
+
+  if (diffDays < 1) {
+    return `${d.format("MMM D")} (${d.fromNow()})`;
+  } else if (diffYears < 1) {
+    return d.format("MMM D");
+  } else {
+    return d.format("MMM D, YYYY");
+  }
+}
